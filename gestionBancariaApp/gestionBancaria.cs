1d2023f6663a791a609a83b716c5a6bb94b5f2c5
DANIEL BRODZEWSKI FERNANDEZ <danbrofer@alu.edu.gva.es>
2023-02-08 18:59:20 +0100

    Métodos de prueba agregados

--- gestionBancariaApp/gestionBancaria.cs
+++ gestionBancariaApp/gestionBancaria.cs
@@ -56,7 +56,7 @@ public class gestionBancaria
         else
         {
             if (cantidad > 0)
-                saldo -= cantidad;
+                saldo += cantidad;
         }
       
     }
---
+++ gestionBancariaTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("gestionBancariaTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("gestionBancariaTest")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("49ccd025-47ed-4dee-ada2-1cf725582d6f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

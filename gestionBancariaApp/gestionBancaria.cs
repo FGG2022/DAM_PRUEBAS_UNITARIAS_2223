8042208e39d8959e51b7cee44e7efdf84d8bc3f6
JJAS2022 <javaliseh@alu.edu.gva.es>
2023-03-03 23:13:05 +0100

    Adición de proyecto de pruebas y modificaciones en el código.

--- gestionBancariaApp/gestionBancaria.cs
+++ gestionBancariaApp/gestionBancaria.cs
@@ -27,38 +27,33 @@ public class gestionBancaria
 
     public void realizarReintegro(double cantidad)
     {
-
         if (cantidad <= 0)
         {
-            mostrarError(ERR_CANTIDAD_INDICADA_NEGATIVA);
+            throw new ArgumentOutOfRangeException("Cantidad indicada es negativa");
         }
         else
         {
-            if (cantidad > 0 && saldo > cantidad)
+            if (cantidad > 0 && saldo >= cantidad)
             {
                 saldo -= cantidad;
                 
             }
             else
-                mostrarError(ERR_SALDO_INSUFICIENTE);
-
+                throw new ArgumentOutOfRangeException("Saldo insuficiente");
         }
- 
     }
 
     public void realizarIngreso(double cantidad)
     {
-
         if (cantidad < 0)
         {
-            mostrarError(ERR_CANTIDAD_INDICADA_NEGATIVA);
+            throw new ArgumentOutOfRangeException("Cantidad indicada es negativa");
         }
         else
         {
             if (cantidad > 0)
-                saldo -= cantidad;
-        }
-      
+                saldo += cantidad;
+        }     
     }
 
 
---
+++ gestionBancariaTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("gestionBancariaTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("gestionBancariaTest")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("920157ba-b9cc-4f21-9fd0-f7844318f7a4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
